7e94676e0d36e9a7ec2db069f5ade00c0d7017c4
diegomestre2 <diegogomest@gmail.com>
2017-04-06 13:00:24 -0300

    CORRECTING SIGNATURE - VERSION TO TESTED

--- trace_generator_tpch_cs_x86.py
+++ trace_generator_tpch_cs_x86.py
@@ -17,25 +17,25 @@ import sys
 DATA_ADDR_READ = 1024 * 1024 * 1024
 DATA_ADDR_WRITE = 1024 * 1024 * 4096
 REGISTER_SIZE = 16
+INSTRUCTION_ADDR = 1024
+DATA_SIZE = 4
 
 BASEDIR = "/Users/diegogomestome/Dropbox/UFPR/Mestrado_Diego_Tome/EXPERIMENTOS/"
 input_file = BASEDIR + "bitmap_files/resultQ06.txt"
 dynamic_trace = BASEDIR + "traces/x86/Q06/columnStore/output_trace.out.tid0.dyn.out"
 memory_trace = BASEDIR + "traces/x86/Q06/columnStore/output_trace.out.tid0.mem.out"
 static_trace = BASEDIR + "traces/x86/Q06/columnStore/output_trace.out.tid0.stat.out"
-
+################### TREATING FILE INPUT ###################
 FILE_INPUT = open(input_file, 'r')
 
 header = FILE_INPUT.readline()
 header = header.split("|")
 numberOfTables = int(header[0])
 numberOfPredicates = int(header[(numberOfTables * 2) + 1])
-INST_ADDR = 1024
-basicBlock = 0
 tuples = FILE_INPUT.readlines()
 
 FILE_INPUT.close()
-
+##########################################################
 qtdTuples = len(tuples)
 w, h = qtdTuples, numberOfPredicates
 dynamic_block = [["" for x in range(w)] for y in range(h)]
@@ -49,15 +49,15 @@ for i in range(numberOfTables):
     totalAttributes[i] = int(header[i + numberOfTables + 1])
 
 AttributesByStage = [totalAttributes, totalAttributes, totalAttributes]
-address_base = [DATA_ADDR_READ, DATA_ADDR_READ + (qtdTuples * REGISTER_SIZE),  # READ
-                DATA_ADDR_READ + (qtdTuples * REGISTER_SIZE * 2)]
+address_base = [DATA_ADDR_READ, DATA_ADDR_READ + (qtdTuples * DATA_SIZE),  # READ
+                DATA_ADDR_READ + (qtdTuples * DATA_SIZE * 2)]
 
-address_target = [DATA_ADDR_WRITE, DATA_ADDR_WRITE + (qtdTuples * REGISTER_SIZE) + 1,  # WRITE
-                  DATA_ADDR_WRITE + (qtdTuples * REGISTER_SIZE * 2) + 1]
+address_target = [DATA_ADDR_WRITE, DATA_ADDR_WRITE + (qtdTuples * DATA_SIZE) + 1,  # WRITE
+                  DATA_ADDR_WRITE + (qtdTuples * DATA_SIZE * 2) + 1]
 
-address_target_bitmap = [int(DATA_ADDR_READ + (qtdTuples * REGISTER_SIZE * 3)),  # BITMAP
-                         int(DATA_ADDR_READ + (qtdTuples * REGISTER_SIZE * 3) + qtdTuples),
-                         int(DATA_ADDR_READ + (qtdTuples * REGISTER_SIZE * 3) + (2 * qtdTuples))]
+address_target_bitmap = [int(DATA_ADDR_READ + (qtdTuples * DATA_SIZE * 3)),  # BITMAP
+                         int(DATA_ADDR_READ + (qtdTuples * DATA_SIZE * 3) + qtdTuples),
+                         int(DATA_ADDR_READ + (qtdTuples * DATA_SIZE * 3) + (2 * qtdTuples))]
 
 
 FILE_STAT = open(static_trace, 'w')
@@ -69,34 +69,34 @@ print "Generating Static File..."
 for tuple in range(numberOfPredicates):
     basicBlock += 1
     FILE_STAT.write("@" + str(basicBlock) + "\n")  # COLUMN-AT-A-TIME#
-    FILE_STAT.write("ADD 1 " + str(INST_ADDR) + " 4 1 5 1 5 0 0 0 0 0 3 0 0 0\n")
-    INST_ADDR += 4
-    FILE_STAT.write("CMP 1 " + str(INST_ADDR) + " 3 1 5 1 6 0 0 0 0 0 3 0 0 0\n")
-    INST_ADDR += 3
-    FILE_STAT.write("JNE 7 " + str(INST_ADDR) + " 2 1 6 1 7 0 0 0 0 0 4 0 0 0\n")
-    INST_ADDR += 2
+    FILE_STAT.write("ADD 1 " + str(INSTRUCTION_ADDR) + " 4 1 5 1 5 0 0 0 0 0 3 0 0 0\n")
+    INSTRUCTION_ADDR += 4
+    FILE_STAT.write("CMP 1 " + str(INSTRUCTION_ADDR) + " 3 1 5 1 6 0 0 0 0 0 3 0 0 0\n")
+    INSTRUCTION_ADDR += 3
+    FILE_STAT.write("JNE 7 " + str(INSTRUCTION_ADDR) + " 2 1 6 1 7 0 0 0 0 0 4 0 0 0\n")
+    INSTRUCTION_ADDR += 2
     basicBlock += 1
     FILE_STAT.write("@" + str(basicBlock) + "\n")  # APPLY PREDICATE)#
-    FILE_STAT.write("CMP 1 " + str(INST_ADDR) + " 4 1 9 1 10 0 0 1 0 0 3 0 0 0\n")  # R 16 Bytes
-    INST_ADDR += 1
-    FILE_STAT.write("JNE 7 " + str(INST_ADDR) + " 2 1 10 1 7 0 0 0 0 0 4 0 0 0\n")
-    INST_ADDR += 2
+    FILE_STAT.write("CMP 1 " + str(INSTRUCTION_ADDR) + " 4 1 9 1 10 0 0 1 0 0 3 0 0 0\n")  # R 16 Bytes
+    INSTRUCTION_ADDR += 1
+    FILE_STAT.write("JNE 7 " + str(INSTRUCTION_ADDR) + " 2 1 10 1 7 0 0 0 0 0 4 0 0 0\n")
+    INSTRUCTION_ADDR += 2
     basicBlock += 1
     FILE_STAT.write("@" + str(basicBlock) + "\n")  # READ BITMAP)#
-    FILE_STAT.write("CMP 1 " + str(INST_ADDR) + " 4 1 13 1 14 0 0 1 0 0 3 0 0 0\n")  # R 1Byte
-    INST_ADDR += 1
-    FILE_STAT.write("JNE 7 " + str(INST_ADDR) + " 2 1 10 1 7 0 0 0 0 0 4 0 0 0\n")
-    INST_ADDR += 2
+    FILE_STAT.write("CMP 1 " + str(INSTRUCTION_ADDR) + " 4 1 13 1 14 0 0 1 0 0 3 0 0 0\n")  # R 1Byte
+    INSTRUCTION_ADDR += 1
+    FILE_STAT.write("JNE 7 " + str(INSTRUCTION_ADDR) + " 2 1 10 1 7 0 0 0 0 0 4 0 0 0\n")
+    INSTRUCTION_ADDR += 2
     basicBlock += 1
     FILE_STAT.write("@" + str(basicBlock) + "\n")  # MATCH POSITION (STORE)#
-    FILE_STAT.write("MOV 9 " + str(INST_ADDR) + " 6 1 10 1 13 0 0 0 0 1 3 0 0 0\n")  # W 1Byte
-    INST_ADDR += 6
-    FILE_STAT.write("ADD 1 " + str(INST_ADDR) + " 4 1 1 1 1 0 0 0 0 0 3 0 0 0\n")
-    INST_ADDR += 4
-    FILE_STAT.write("CMP 1 " + str(INST_ADDR) + " 3 1 1 1 2 0 0 0 0 0 3 0 0 0\n")
-    INST_ADDR += 3
-    FILE_STAT.write("JNE 7 " + str(INST_ADDR) + " 2 1 2 1 3 0 0 0 0 0 4 0 0 0\n")
-    INST_ADDR += 2
+    FILE_STAT.write("MOV 9 " + str(INSTRUCTION_ADDR) + " 6 1 10 1 13 0 0 0 0 1 3 0 0 0\n")  # W 1Byte
+    INSTRUCTION_ADDR += 6
+    FILE_STAT.write("ADD 1 " + str(INSTRUCTION_ADDR) + " 4 1 1 1 1 0 0 0 0 0 3 0 0 0\n")
+    INSTRUCTION_ADDR += 4
+    FILE_STAT.write("CMP 1 " + str(INSTRUCTION_ADDR) + " 3 1 1 1 2 0 0 0 0 0 3 0 0 0\n")
+    INSTRUCTION_ADDR += 3
+    FILE_STAT.write("JNE 7 " + str(INSTRUCTION_ADDR) + " 2 1 2 1 3 0 0 0 0 0 4 0 0 0\n")
+    INSTRUCTION_ADDR += 2
 
 FILE_STAT.write("# eof")
 FILE_STAT.close()
@@ -109,7 +109,6 @@ FILE_MEM.write("# SiNUCA Trace Memory\n")
 
 fieldsByInstruction = REGISTER_SIZE / 4
 lastFieldSum = 0
-countLoads = 0
 loadCount = [0, 0, 0]
 #################### DYNAMIC AND MEMORY FILE #########################
 print "Generating Data For Dynamic and Memory Files..."
@@ -160,7 +159,7 @@ for tuple in range(len(tuples)):
                 if column == numberOfPredicates - 1:
                     fieldsByInstruction = REGISTER_SIZE / 4 + 1
                 if lastFieldSum > 0:
-                    countLoads += 1
+                    lastFieldSum = 0
                     dynamic_block[column][tuple] += str(str(basicBlock + 2) + "\n")
                     memory_block[column][tuple] += (
                         "R " + str(REGISTER_SIZE) + " " + str(address_base[column]) + " " + str(
@@ -172,7 +171,6 @@ for tuple in range(len(tuples)):
                         ########################################################################
                         ## CREATE THE BITMAP 1 Byte of Store by 32 Bytes of Loads
                         ########################################################################
-
                         dynamic_block[column][tuple] += str(str(basicBlock + 3) + "\n")
                         dynamic_block[column][tuple] += str(str(basicBlock + 4) + "\n")
                         memory_block[column][tuple] += (
